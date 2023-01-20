4bee727f5abeb03f5a40fda16976267321bb2d57
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-01-20 14:00:43 +0200

    Added media for wrapper

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ let wrap = document.querySelector(".wrapper");
 let ball = document.querySelector(".box");
 let click = 0;
 
+
 function moveBall(e) {
   ball.style.transform = `translate(${e.clientX - 50}px,
          ${e.clientY - 50}px)`;
